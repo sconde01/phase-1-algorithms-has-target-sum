fc9b201fba6b52f65dd35ef309d0227624037bd0
Stephanie Conde <stephaniecondecareer@gmail.com>
2022-05-09 20:36:15 -0700

    commit tree 0960963025239aed04e17cdadf2ea6bda61ddeed

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for (let i = 0; i <array.length; i++){
+    const complement = target - array[i];
+    for (let j =i +1; j < array.length; j++){
+      if (array[j] === complement) 
+    
+      return true;
+    }
+  }
+  return false;
 }
 
 /* 
@@ -7,7 +16,13 @@ function hasTargetSum(array, target) {
 */
 
 /* 
-  Add your pseudocode here
+  Add your pseudocode here iterate over the array of numbers
+  for the current number, identify a complementary number that adds to our target
+  (for example: if our number is 2, and the target is 5, the complementary number is 3)
+  iterate over the remaining numbers in the array
+    check if any of the remaining numbers is the complement
+      if so, return true
+if we reach the end of the array, return false
 */
 
 /*
